0e802ce0417b8f1647378099a0a0258d30e2249b
bryllim <bryllim@gmail.com>
2023-01-31 14:50:10 +0800

    separeted main.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,35 +40,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
-    <script>
-    const dataTable = new DataTable("#usermanagement");
-
-    const editUserModal = new bootstrap.Modal('#editusermodal', {
-        keyboard: false
-    });
-    
-    function showUser(user_id){
-        fetch('{{ url('/users/') }}/' + user_id)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('edituser_email').value = data.email;
-            document.getElementById('edituser_name').value = data.name;
-            document.getElementById('edituser_id').value = data.id;
-            editUserModal.show();
-        })
-    }
-
-    function imageUpload(event){
-        let imageInput = document.getElementById('image');
-        document.getElementById("imageUploadLabel").remove();
-
-        let img = new Image();
-        img.src = URL.createObjectURL(event.target.files[0]);
-        img.classList.add("img-thumbnail");
-        img.classList.add("mt-2");
-        document.getElementById('imageArea').appendChild(img);
-    }
-    </script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
 
 </html>
